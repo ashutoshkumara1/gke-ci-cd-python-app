17bcca9b6861b6d8f24298d1f79f5b8b9e071d09
Ashutosh Kumar <ashutoshkumara1@gmail.com>
2022-09-26 15:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
-app = Flask('hello-world')
+from flask import Flask, render_template
+app = Flask(__name__)
+
 @app.route('/')
 def hello():
- return "The Container Deployed On Kubernetes Cluster\n and Running it On Server!"
+   return render_template('index.html')
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   # Load current count
+   f = open("count.txt", "r")
+   count = int(f.read())
+   f.close()
+   # Increment the count
+   count += 1
+   # Overwrite the count
+   f = open("count.txt", "w")
+   f.write(str(count))
+   f.close()
+
+   return render_template('hello.html', name=name, count=count)
+
 if __name__ == '__main__':
- app.run(host = '0.0.0.0', port = 8080)
+   app.run(host ='0.0.0.0', debug = True)
